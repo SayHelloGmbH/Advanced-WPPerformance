b67acf1e8906cfa3904f87ef19bba09446e59991
nico_martin <mail@nicomartin.ch>
2017-09-28 08:11:57 +0200

    strange bug, thank you @gnanet

--- advanced-wpperformance.php
+++ advanced-wpperformance.php
@@ -5,7 +5,7 @@ Plugin Name: Advanced WPPerformance
 Plugin URI: https://github.com/nico-martin/Advanced-WPPerformance
 Description: This plugin adds several performance improvements to your WordPress site
 Author: Nico Martin
-Version: 1.2.2-dev
+Version: 1.2.3-dev
 Author URI: https://nicomartin.ch
 Text Domain: awpp
 Domain Path: /languages
@@ -66,10 +66,6 @@ if ( version_compare( $wp_version, '4.7', '<' ) || version_compare( PHP_VERSION,
 	require_once 'Classes/class-criticalcss.php';
 	awpp_get_instance()->CriticalCSS = new nicomartin\AdvancedWPPerformance\CriticalCSS();
 	awpp_get_instance()->CriticalCSS->run();
-
-	require_once 'Classes/class-http2push.php';
-	awpp_get_instance()->Http2Push = new nicomartin\AdvancedWPPerformance\Http2Push();
-	awpp_get_instance()->Http2Push->run();
 
 	require_once 'Classes/class-http2push.php';
 	awpp_get_instance()->Http2Push = new nicomartin\AdvancedWPPerformance\Http2Push();
